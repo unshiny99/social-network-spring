d7a7792b95a1ef2460f1e0a7bb30cde2a37f062a
Maxime Frémeaux <maxime.fremeaux2@gmail.com>
2022-12-29 18:25:29 +0100

    add Basic Auth for all requests

--- src/main/java/com/bd/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/com/bd/socialnetwork/SocialNetworkApplication.java
@@ -15,6 +15,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.io.FileReader;
 import java.util.ArrayList;
@@ -56,6 +59,6 @@ public class SocialNetworkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialNetworkApplication.class, args);
-		loadData();
+		//loadData();
 	}
 }
